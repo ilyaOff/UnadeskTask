9ca0ed01ae74677dd90ca81a8c0263483c5d816e
Ilya <ilyaboomer@mail.ru>
2026-05-15 22:48:58 +0300

    feat: более реалистичный обработчик PDF

--- src/BackgroundWorker.App/Services/FakePdfTextExtractor.cs
+++ src/BackgroundWorker.App/Services/FakePdfTextExtractor.cs
@@ -4,6 +4,8 @@ public class FakePdfTextExtractor : IPdfTextExtractor
 {
 	public async IAsyncEnumerable<PageText> ExtractTextAsync(Stream pdfStream, CancellationToken cancellationToken = default)
 	{
+		yield return new PageText(1, "Страница 1", 2);
+		yield return new PageText(2, "Страница 2", 2);
 		yield break;
 	}
 }
